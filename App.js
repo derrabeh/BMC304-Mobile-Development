0e39ab2d6732b61f23bfdae0c8df336131ebc493
Daenise <daenise@easystore.co>
2019-03-20 02:21:55 +0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import firebase from 'firebase';
 import { createStackNavigator, createAppContainer } from 'react-navigation'; 
 import { AppDrawerNavigator } from './src/navigation/DrawerNavigator';
-import { LoginScreen, SignUpScreen, UniHomeScreen, AdminHomeScreen, DetailsScreen,
+import { LoginScreen, SignUpScreen, UniHomeScreen, AdminHomeScreen, HomeScreen, DetailsScreen,
           QualificationScreen, StudentApplicationScreen } from './src/screen';
 
 
@@ -32,14 +32,14 @@ const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
   componentWillMount() {
-    // Initialize 
+    // Initialize Firebase
     firebase.initializeApp({
-      apiKey: 'AIzaSyAVNZrTYMo4YKjaLT64g-K4FWUVr8LkUH8',
-      authDomain: 'uni-search-367ae.firebaseapp.com',
-      databaseURL: 'https://uni-search-367ae.firebaseio.com',
-      projectId: 'uni-search-367ae',
-      storageBucket: 'uni-search-367ae.appspot.com',
-      messagingSenderId: '333300685077'
+    apiKey: "AIzaSyAVNZrTYMo4YKjaLT64g-K4FWUVr8LkUH8",
+    authDomain: "uni-search-367ae.firebaseapp.com",
+    databaseURL: "https://uni-search-367ae.firebaseio.com",
+    projectId: "uni-search-367ae",
+    storageBucket: "uni-search-367ae.appspot.com",
+    messagingSenderId: "333300685077"
     });
   }
 
