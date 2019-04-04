eedda9e06c61d8e4d39fa02f7c10afb20c4f7d74
Yc <>
2019-04-04 21:33:02 +0800

    ua-func done

--- src/screen/AppDetailScreen.js
+++ src/screen/AppDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, ToastAndroid } from 'react-native';
+import { View, Text, Button, ToastAndroid, TouchableOpacity } from 'react-native';
 import { Header, Input } from '../components/common';
 import firebase from 'firebase';
 
@@ -11,7 +11,7 @@ class AppDetails extends React.Component {
     chgApplicantStatus(status,key){
       console.log(status,key,'www')
       try {
-        var applicant = firebase.database().ref('application_test/'+key);
+        var applicant = firebase.database().ref('applicant_new/'+key);
         applicant.update({ status: status});
         ToastAndroid.show('Status Updated!', ToastAndroid.SHORT);
         setTimeout(()=>{
@@ -29,27 +29,35 @@ class AppDetails extends React.Component {
       let d = this.props.navigation;
       return (
         <View>
-            <Header headerText={'Applicant Details'} navigation={this.props.navigation} />
-            <Text>Applicant Details {'\n'}</Text>
+          <Header headerText={'Applicant Details'} navigation={this.props.navigation} />
+          <View style={styles.container}>
+          <Text>Applicant Details {'\n'}</Text>
 
-            <Text>Applicant: {d.state.params.applicant}</Text>
-            <Text>Status: {d.state.params.status}</Text>
-            <Text>Apply Programme: {d.state.params.prog_name}</Text>
+              <Text>Applicant: {d.state.params.applicant}{'\n'}</Text>
+              <Text>Status: {d.state.params.status}{'\n'}</Text>
+              <Text>Apply Programme: {d.state.params.prog_name}{'\n'}</Text>
+              <Text>Key : {d.state.params.key}</Text>
 
-            <Button title="Approve" color="green" onPress={() => this.chgApplicantStatus('APPROVE',d.state.params.key)}/>
-            <Button title="Decline" color="red" onPress={() => this.chgApplicantStatus('DECLINE',d.state.params.key)}/>
+              <Button title="Approve" color="green" onPress={() => this.chgApplicantStatus('APPROVE',d.state.params.key)}/>
+              <Button title="Decline" color="red" onPress={() => this.chgApplicantStatus('DECLINE',d.state.params.key)}/>
 
-            <Text>{'\n'}</Text>
-
-
-
-
-
-
-            <Button title="Back" onPress={() => this.props.navigation.push('App_Prog',{ prog_id : d.state.params.prog_id,prog_name : d.state.params.prog_name, })} />
+              <Text>{'\n'}</Text>
+              <Button title="Back" onPress={() => this.props.navigation.push('App_Prog',{ prog_id : d.state.params.prog_id,prog_name : d.state.params.prog_name, })} />
+          </View>
         </View>
       );
     }
 }
+
+const styles = {
+  container : {
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius : 10,
+    padding : 20,
+    backgroundColor : '#f4f6f9',
+  }
+};
 
 export { AppDetails };
